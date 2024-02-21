b4c762538d93ec87e19088165a0051d1baf6de30
Ephe <53063515+EpheDaBeef@users.noreply.github.com>
2024-02-20 19:21:46 -0800

    Add files via upload

---
+++ can_test/can_test.ino
@@ -0,0 +1,61 @@
+#include <SPI.h>
+#define CAN_2515
+// #define CAN_2518FD
+// Set SPI CS Pin according to your hardware
+#if defined(SEEED_WIO_TERMINAL) && defined(CAN_2518FD)
+// For Wio Terminal w/ MCP2518FD RPi Hat：
+// Channel 0 SPI_CS Pin: BCM 8
+// Channel 1 SPI_CS Pin: BCM 7/Users/ephraimjosephdelacruz/Downloads/mcp2518fd_can.h
+// Interupt Pin: BCM25
+const int SPI_CS_PIN  = BCM8;
+const int CAN_INT_PIN = BCM25;
+#else
+// For Arduino MCP2515 Hat:
+// the cs pin of the version after v1.1 is default to D9
+// v0.9b and v1.0 is default D10
+const int SPI_CS_PIN = 9;
+const int CAN_INT_PIN = 2;
+int hi = 0;
+#endif
+#ifdef CAN_2518FD
+#include "mcp2518fd_can.h"
+mcp2518fd CAN(SPI_CS_PIN); // Set CS pin
+#endif
+#ifdef CAN_2515
+#include "mcp2515_can.h"
+mcp2515_can CAN(SPI_CS_PIN); // Set CS pin
+#endif
+void setup() {
+    SERIAL_PORT_MONITOR.begin(115200);
+    while(!Serial){};
+    while (CAN_OK != CAN.begin(CAN_500KBPS)) {             // init can bus : baudrate = 500k
+        SERIAL_PORT_MONITOR.println("CAN init fail, retry...");
+        delay(100);
+    }
+    SERIAL_PORT_MONITOR.println("CAN init ok!");
+}
+void loop() {
+    delay(100);                       // send data per 1000ms
+    hi = hi + 1;
+    unsigned char stmp[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+    unsigned char len = 8;
+    unsigned char buf[8];
+    SERIAL_PORT_MONITOR.println("-");
+    if (CAN_MSGAVAIL == CAN.checkReceive()) {         // check if data coming
+        CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+        unsigned long canId = CAN.getCanId();
+        SERIAL_PORT_MONITOR.println("-----------------------------");
+        SERIAL_PORT_MONITOR.print("Get data from ID: 0x");
+        SERIAL_PORT_MONITOR.println(canId, HEX);
+        for (int i = 0; i < len; i++) { // print the data
+            SERIAL_PORT_MONITOR.print(buf[i], HEX);
+            SERIAL_PORT_MONITOR.print("\t");
+        }
+        SERIAL_PORT_MONITOR.println();
+    }
+    while(1){
+      SERIAL_PORT_MONITOR.println("sending...");
+      CAN.sendMsgBuf(0x190, 0, 8, stmp);
+      stmp[0]++;
+  }
+}
