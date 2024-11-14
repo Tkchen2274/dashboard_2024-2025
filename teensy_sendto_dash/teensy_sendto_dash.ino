707e78d0e13cf5a2c8ace604f128ced231244f68
Lou1e282 <louieshe@usc.edu>
2024-11-13 19:39:52 -0800

    VCU-teensy-response

--- teensy_sendto_dash/teensy_sendto_dash.ino
+++ teensy_sendto_dash/teensy_sendto_dash.ino
@@ -1,3 +1,8 @@
+#include <FlexCAN.h>
+
+// Define the CAN bus settings
+FlexCAN CANbus(500000);  // CAN bus speed: 500 kbps
+
 int i = 0;
 int speed[5] = {1, 2, 3, 4, 5};
 int bat[5] = {100, 99, 98, 97 ,96};
@@ -5,8 +10,17 @@ int temp[5] = {37, 38, 39, 40, 41};
 
 void setup() {
   // Start serial communication with Nextion display
-  Serial.begin(9600);
-  Serial2.begin(9600);
+  //Serial.begin(9600);
+  Serial2.begin(9600);    // RXTX
+
+  // Can communication
+  Serial.begin(115200);   // CAN communication
+  while (!Serial); // Wait for Serial monitor to open
+  Serial.println("Starting CAN Bus...");
+
+  // Start the CAN bus
+  CANbus.begin();
+  Serial.println("CAN Bus initialized.");
 }
 
 void loop() {
@@ -22,8 +36,20 @@ void loop() {
   sendNumberToNextion("numtemp", temp[i]);
   sendNumberToNextion("t4", ctof(temp[i]));
 
+   CAN_message_t rxMsg; // Message object to store received CAN data
+
+  // Check if a new CAN message has been received
+  if (CANbus.read(rxMsg)) {
+    bool buzz = false;
+    if (rxMsg[1] == 0x902) {
+      buzz = true;
+    }
+    // sendResponse(rxMsg, rxMsg[1] == 0x902);
+    sendResponse(rxMsg, buzz);
+  }
+
   // Delay for a second
-  delay(1000);
+  //delay(1000);  delay used for dashboard refresh
 }
 
 float ctof (int c) {
@@ -41,6 +67,15 @@ void sendNumberToNextion(String component, float value) {
   Serial2.print("\"");          // End the string value
 
   sendEndCommand();
+}
+
+void sendResponse(CAN_message_t rxMsg, bool buzz) {
+  CANbus.write(rxMsg);
+  if (buzz) {
+    int buzzer = 0x903; // or another number, unique for use
+    CANbus.write(buzzer); // some code to make buzz
+    Serial.write(Buzzed); // Validation passed
+  }
 }
 
 // Function to send the end command required by Nextion
